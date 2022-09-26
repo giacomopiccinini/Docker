1e83014d9d3da0d93b742f1167a56ad417d7f793
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-09-26 15:03:47 +0200

    [DEL] Remove DeepLearning image

--- DeepLearning/Scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Build Docker image named my_image from Dockerfile
-docker image build --tag my_image .
--- DeepLearning/Scripts/remove.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Remove container
-docker container rm -f my_container
